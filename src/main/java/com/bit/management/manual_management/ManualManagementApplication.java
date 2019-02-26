705c42ad64dddd2f1e6e4a0e130cb55230646014
shixi_xiaoying7 <shixi_xiaoying7@staff.weibo.com>
2019-02-26 15:10:55 +0800

    feat: first commit

---
+++ src/main/java/com/bit/management/manual_management/ManualManagementApplication.java
@@ -0,0 +1,14 @@
+package com.bit.management.manual_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//包扫描
+@SpringBootApplication
+public class ManualManagementApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ManualManagementApplication.class, args);
+  }
+
+}
---
+++ src/main/java/com/bit/management/manual_management/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.bit.management.manual_management.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/welcome")
+public class HelloController {
+
+  @RequestMapping(value = "/home")
+  public String hello(){
+    return "he.html";
+  }
+
+}
